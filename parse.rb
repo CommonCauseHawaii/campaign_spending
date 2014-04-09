a69422f3599e6302994121f3355c1531e4ad87cf
Jason Axelson <jason.axelson@gmail.com>
2014-04-08 19:52:53 -1000

    Code to correctly parse csv from socrata

--- parse.rb
+++ parse.rb
@@ -1,18 +1,40 @@
 require 'csv'
 new_data = []
 id = 1
-CSV.open('header.csv', 'w',
+CSV.open('expenditures_parsed.csv', 'w',
          :write_headers => true,
-         :headers => %w(candidate_name expenditure_category election_period office amount id)) do |output_csv|
-           CSV.foreach("_jekyll/data/campaign_spending_summary.csv", :headers => true) do |row|
-             puts "on row #{row}"
-             row['id'] = id
-             row['amount'] = row['amount'][1..-1]
-             output_csv << row
-             # use row here...
+         :headers => %w(expenditure_category election_period amount reg_no id)) do |output_csv|
+           CSV.foreach("expenditures.csv", :headers => true) do |row|
+             puts "on row #{row.inspect}"
+             entry = {
+               expenditure_category: row['Expenditure Category'],
+               election_period: row['Election Period'],
+               amount: row['Amount'][1..-1],
+               reg_no: row['Reg No'],
+               id: id,
+             }
+             output_csv << entry.values
              id += 1
            end
          end
+
+CSV.open('organizational_reports_parsed.csv', 'w',
+         :write_headers => true,
+         :headers => %w(reg_no candidate_name office district county party)) do |output_csv|
+           CSV.foreach("organizational_reports.csv", :headers => true) do |row|
+             puts "on row #{row.inspect}"
+             entry = {
+               reg_no: row['Reg No'],
+               candidate_name: row['Candidate Name'],
+               office: row['Office'],
+               district: row['District'],
+               county: row['County'],
+               party: row['Party']
+             }
+             output_csv << entry.values
+           end
+         end
+
 #CSV.foreach("_jekyll/data/campaign_spending_summary.csv.short") do |row|
 #  puts "on row #{row}"
 #  row['id'] = id if id != 0
