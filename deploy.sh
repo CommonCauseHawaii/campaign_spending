c07c49d3f5ae251aea1e72803b2f37c28c42c475
Jason Axelson <jason.axelson@gmail.com>
2014-04-14 19:23:52 -1000

    First stab at deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git diff --exit-code >/dev/null
+if [ $? -ne 0 ]; then
+    echo "Requires a clean git directory"
+    exit 1
+fi
+
+cp -r _site ../site-bak
+git checkout gh-pages
+rm *
+mv ../site-bak/* .
+git add .
