2efb6da5bdb22ff668ef6cebf4654b4bb284b0d1
Jason Axelson <jason.axelson@gmail.com>
2014-04-20 00:53:51 -1000

    Mac notifier for jenkins
    requires gem terminal-notifier terminal-notifier-guard

---
+++ _jekyll/_plugins/ext.rb
@@ -0,0 +1,25 @@
+# This monkey patch triggers a Mac OS X Notification Center notification using
+# the terminal-notifier gem.
+#
+# If you start jekyll without bundle, first install the necessary gems:
+#
+# gem install terminal-notifier
+# gem install terminal-notifier-guard
+#
+# If you start jekyll with bundle exec, ensure the above two gems are in
+# your Gemfile:
+#
+# gem 'terminal-notifier'
+# gem 'terminal-notifier-guard' 
+ 
+module Jekyll
+  class Site
+    alias old_write write
+    def write
+      old_write
+      if `uname`.strip == "Darwin"
+        `terminal-notifier-success -title "#{config['title'] ? config['title'] : 'Jekyll Site'}" -message "Jekyll generate complete."`
+      end
+    end
+  end
+end
